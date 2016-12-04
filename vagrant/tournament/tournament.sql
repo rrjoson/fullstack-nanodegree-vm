fac73fe77c9d3153162392d885f60c2b8f5d8157
rrjoson <rr_joson@yahoo.com>
2016-12-05 00:12:03 +0800

    feat: Setup database first before creating table schema

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,8 +6,9 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-DROP TABLE IF EXISTS matches;
-DROP TABLE IF EXISTS players;
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament
 
 CREATE TABLE players ( id SERIAL PRIMARY KEY,
 						name TEXT );
