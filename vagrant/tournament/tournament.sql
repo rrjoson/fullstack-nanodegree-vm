532cc2a7e55ecc932330cf8811a58f3c4e0dcd38
rrjoson <rr_joson@yahoo.com>
2016-12-05 04:16:19 +0800

    feat: Add view for player standings

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -17,3 +17,39 @@ CREATE TABLE matches ( id SERIAL PRIMARY KEY,
 						winner INTEGER REFERENCES players (id),
 						loser INTEGER REFERENCES players (id));
 
+CREATE VIEW total_matches AS (
+	SELECT
+		players.id,
+		count(matches.id) AS total_matches
+	FROM matches
+	INNER JOIN players
+	ON players.id=matches.winner
+	OR players.id=matches.loser
+	GROUP BY players.id
+);
+
+CREATE VIEW wins AS (
+	SELECT
+		players.id,
+		count(matches.id) AS wins
+	FROM players
+	INNER JOIN matches
+	ON players.id=matches.winner
+	GROUP BY players.id
+);
+
+
+CREATE VIEW standings AS (
+	SELECT
+		players.id,
+		players.name,
+		coalesce(total_matches.total_matches, 0) AS total_matches,
+		coalesce(wins.wins ,0) AS wins
+	FROM players
+	LEFT JOIN total_matches
+	ON players.id=total_matches.id
+	LEFT JOIN wins
+	ON wins.id=players.id
+	ORDER BY wins.wins
+	DESC
+);
